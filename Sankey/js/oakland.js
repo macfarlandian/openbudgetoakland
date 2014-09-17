3779632338d3c87181f987883fd704e0a7a22ac9
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-09-17 09:56:49 -0700

    switch between years

--- Sankey/js/oakland.js
+++ Sankey/js/oakland.js
@@ -21,16 +21,6 @@ svg.append("text")
     .attr("x", margin.left + width)
     .attr("text-anchor", "end");
 
-var chart = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var sankey = d3.sankey()
-    .nodeWidth(20)
-    .nodePadding(10)
-    .size([width, height]);
-
-var path = sankey.link();
-
 // define color scales
 var fundColors = d3.scale.ordinal()
     .domain(["General Fund", "Non-discretionary funds"])
@@ -91,124 +81,11 @@ svg.append('linearGradient')
     .attr("offset", function(d) { return d.offset; })
     .attr("stop-color", function(d) { return d.color; });
 
-function do_with_budget(data) {
-
-  sankey
-      .nodes(data.nodes)
-      .links(data.links)
-      .layout(0);
-
-  var link = chart.append("g").selectAll(".link")
-      .data(data.links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) {
-          return Math.max(1, d.dy); })
-      .style("stroke", function(d){
-          switch (d.target.name){
-              case "General Fund":
-                  return "url('#gradientRtoGF')";
-              case "Non-discretionary funds":
-                  return "url('#gradientRtoNF')";
-          }
-          switch (d.source.name) {
-              case "General Fund":
-                  return "url('#gradientGFtoE')";
-              case "Non-discretionary funds":
-                  return "url('#gradientNFtoE')";
-          }
-
-      })
-      .sort(function(a, b) {
-          return b.dy - a.dy;
-          })
-      .on("mouseover", function(d){
-        d3.select(this).classed("highlight", true);
-        d3.select("#hover_description")
-            .classed("show", true)
-            .text(d.source.name + " → " + d.target.name + ": " + format(d.value));
-      })
-      .on("mousemove", function(d){
-          d3.select("#hover_description")
-            .style({
-                    "top": (d3.event.y - 10 + $(window).scrollTop()) + "px",
-                    "left": (d3.event.x + 10) + "px"
-                    });
-      })
-      .on("mouseout", function(){
-        d3.select(this).classed("highlight", function(){
-            return d3.select(this).classed('click');
-        })
-        d3.select("#hover_description")
-            .classed("show", false);
-      });
-
-  var node = chart.append("g").selectAll(".node")
-      .data(data.nodes)
-    .enter().append("g")
-      .attr("class", function(d){ return "node " + d.type; })
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-
-  node.append("rect")
-      .attr("height", function(d) { return d.dy; })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) {
-          switch (d.type) {
-              case "fund":
-                  d.color = fundColors(d.name);
-                  break;
-              default:
-                  d.color = erColors(d.type);
-                  break;
-          }
-          return d.color;
-      })
-      .style("stroke", function(d) { return d3.rgb(d.color).darker(1); })
-      .on("click", function(d){
-          var thisnode = d3.select(this.parentNode);
-          if (thisnode.classed("highlight")) {
-              thisnode.classed("highlight", false);
-          } else {
-            //   node.classed("highlight", false);
-              thisnode.classed("highlight", true);
-          }
-
-          link.classed('highlight click', function(link_d){
-              if ([link_d.target.name, link_d.source.name].indexOf(d.name) >= 0) {
-                  return thisnode.classed("highlight");
-              } else {
-                  return d3.select(this).classed("click");
-              }
-          });
-
-          thisnode.select("text").transition()
-            .text(function(d){
-                var text = d.name;
-                if (thisnode.classed('highlight')) {
-                    text += ': ' + format(d.value);
-                }
-                return text;
-            });
-      });
-
-  node.append("text")
-      .attr("x", -6)
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function(d) { return d.name; })
-    .filter(function(d) { return d.x < width / 2; })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
-};
-
-// new data
-d3.csv("City_of_Oakland_Adopted_Policy_Budget_FY13-15.csv", function(error, data){
-    // console.log("loaded");
-    newdata = data.filter(function(v){ return v.budget_year == "FY13-14"});
+// wrangle the data
+function data_wrangle(dataset, fy){
+    var newdata = dataset.filter(function(v){
+        return v.budget_year == fy
+    })
     rev_order = ["Property Tax", "Business License Tax", "Sales Tax",
         "Utility Consumption Tax", "Real Estate Transfer Tax",
         "Fines & Penalties", "Parking Tax", "Transient Occupancy Tax",
@@ -322,6 +199,144 @@ d3.csv("City_of_Oakland_Adopted_Policy_Budget_FY13-15.csv", function(error, data
         }
     }
 
-    oakland_data = {"nodes": nodes, "links": links};
-    do_with_budget(oakland_data);
+    return {"nodes": nodes, "links": links};
+}
+
+// render the sankey
+function do_with_budget(data) {
+
+  svg.select("#chart").remove();
+
+  var chart = svg.append("g")
+      .attr("id", "chart")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var sankey = d3.sankey()
+      .nodeWidth(20)
+      .nodePadding(10)
+      .size([width, height]);
+
+  var path = sankey.link();
+
+  sankey
+      .nodes(data.nodes)
+      .links(data.links)
+      .layout(0);
+
+  var link = chart.append("g")
+    .selectAll(".link")
+      .data(data.links)
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", path)
+      .style("stroke-width", function(d) {
+          return Math.max(1, d.dy); })
+      .style("stroke", function(d){
+          switch (d.target.name){
+              case "General Fund":
+                  return "url('#gradientRtoGF')";
+              case "Non-discretionary funds":
+                  return "url('#gradientRtoNF')";
+          }
+          switch (d.source.name) {
+              case "General Fund":
+                  return "url('#gradientGFtoE')";
+              case "Non-discretionary funds":
+                  return "url('#gradientNFtoE')";
+          }
+
+      })
+      .sort(function(a, b) {
+          return b.dy - a.dy;
+          })
+      .on("mouseover", function(d){
+        d3.select(this).classed("highlight", true);
+        d3.select("#hover_description")
+            .classed("show", true)
+            .text(d.source.name + " → " + d.target.name + ": " + format(d.value));
+      })
+      .on("mousemove", function(d){
+          d3.select("#hover_description")
+            .style({
+                    "top": (d3.event.y - 10 + $(window).scrollTop()) + "px",
+                    "left": (d3.event.x + 10) + "px"
+                    });
+      })
+      .on("mouseout", function(){
+        d3.select(this).classed("highlight", function(){
+            return d3.select(this).classed('click');
+        })
+        d3.select("#hover_description")
+            .classed("show", false);
+      });
+
+  var node = chart.append("g").selectAll(".node")
+      .data(data.nodes)
+    .enter().append("g")
+      .attr("class", function(d){ return "node " + d.type; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+
+  node.append("rect")
+      .attr("height", function(d) { return d.dy; })
+      .attr("width", sankey.nodeWidth())
+      .style("fill", function(d) {
+          switch (d.type) {
+              case "fund":
+                  d.color = fundColors(d.name);
+                  break;
+              default:
+                  d.color = erColors(d.type);
+                  break;
+          }
+          return d.color;
+      })
+      .style("stroke", function(d) { return d3.rgb(d.color).darker(1); })
+      .on("click", function(d){
+          var thisnode = d3.select(this.parentNode);
+          if (thisnode.classed("highlight")) {
+              thisnode.classed("highlight", false);
+          } else {
+            //   node.classed("highlight", false);
+              thisnode.classed("highlight", true);
+          }
+
+          link.classed('highlight click', function(link_d){
+              if ([link_d.target.name, link_d.source.name].indexOf(d.name) >= 0) {
+                  return thisnode.classed("highlight");
+              } else {
+                  return d3.select(this).classed("click");
+              }
+          });
+
+          thisnode.select("text").transition()
+            .text(function(d){
+                var text = d.name;
+                if (thisnode.classed('highlight')) {
+                    text += ': ' + format(d.value);
+                }
+                return text;
+            });
+      });
+
+  node.append("text")
+      .attr("x", -6)
+      .attr("y", function(d) { return d.dy / 2; })
+      .attr("dy", ".35em")
+      .attr("text-anchor", "end")
+      .attr("transform", null)
+      .text(function(d) { return d.name; })
+    .filter(function(d) { return d.x < width / 2; })
+      .attr("x", 6 + sankey.nodeWidth())
+      .attr("text-anchor", "start");
+};
+
+d3.csv("City_of_Oakland_Adopted_Policy_Budget_FY13-15.csv", function(error, data){
+    var oakland_data = [];
+    oakland_data["FY13-14"] = data_wrangle(data, "FY13-14");
+    do_with_budget(oakland_data["FY13-14"]);
+    oakland_data["FY14-15"] = data_wrangle(data, "FY14-15");
+    $('#fy').on("change", function(){
+        do_with_budget(oakland_data[this.value]);
+    })
 });
